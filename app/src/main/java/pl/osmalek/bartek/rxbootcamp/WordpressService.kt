cc3e953373feada4ecd8417a979ff75c8b315d3d
nonda <nonda95@gmail.com>
2017-09-20 17:15:24 +0200

    Added author cache

--- app/src/main/java/pl/osmalek/bartek/rxbootcamp/WordpressService.kt
+++ app/src/main/java/pl/osmalek/bartek/rxbootcamp/WordpressService.kt
@@ -1,7 +1,10 @@
 package pl.osmalek.bartek.rxbootcamp
 
+import android.util.Log
+import android.util.LruCache
 import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.rxkotlin.toSingle
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -10,6 +13,7 @@ import java.util.concurrent.TimeUnit
 const val BASE_URL = "https://thedroidsonroids.com/wp-json/wp/v2/"
 
 class WordpressService {
+    val cache = LruCache<Long, User>(20)
     private val wordpress: Wordpress = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create())
@@ -21,7 +25,7 @@ class WordpressService {
         return wordpress.getPosts(search.toString())
                 .flatMapObservable { Observable.fromIterable(it) }
                 .flatMapSingle { post ->
-                    getUser(post)
+                    getUser(post.author)
                             .map { (name) ->
                                 PostWithAuthor(post.slug, name)
                             }
@@ -30,5 +34,9 @@ class WordpressService {
                 .timeout(10, TimeUnit.SECONDS)
     }
 
-    private fun getUser(it: Post) = wordpress.getUser(it.author)
+    private fun getUser(id: Long) = cache[id]?.logCacheHit()?.toSingle()
+            ?: wordpress.getUser(id).doOnSuccess { cache.put(id, it) }
+
+    private fun User.logCacheHit(): User = apply { Log.d(WordpressService::class.java.simpleName, "Cache hit: $name") }
+
 }
